8a6baa8a5c2a5e6ea6d34b0d52dd7a6b4f7f9a74
Macarena Lara Molina <molina.macarenalara@gmail.com>
2022-05-14 00:39:09 -0300

    se sube el nav bar
    componente nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>Pichulina Bakery y Café</h1>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Tienda</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
