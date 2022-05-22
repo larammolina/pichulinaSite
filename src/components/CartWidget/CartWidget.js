08d72c4e56647fffa8762d45187c19a9e33fa3c5
Macarena Lara Molina <molina.macarenalara@gmail.com>
2022-05-22 20:22:41 -0300

    se corrige la imagen del cart
    se comenta el import de react

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,9 +1,10 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 const CartWidget = () => {
     return (
-        <FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+        //<FontAwesomeIcon icon="fa-solid fa-cart-shopping" />
+        <img src='/images/cart.jpg' alt='cart-widget'/>
     )
 }
 
