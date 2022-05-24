d58d7f04e9628b508641b49d99ac88b553dcd75c
Macarena Lara Molina <molina.macarenalara@gmail.com>
2022-05-23 22:37:02 -0300

    se sube el componente item count
    suma, resta y agrega al carrito

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+const ItemCount = ({ initial = 1, stock = 0, onAdd }) => {
+    const [ count, setCount ] = useState(initial)
+
+    const decrement = () => {
+        if (count > 1){
+            setCount(count - 1)
+        }
+    }
+
+    const increment = () => {
+        if (count < stock){
+            setCount(count + 1)
+        }
+    }
+
+    return (
+        <div>
+            <div> 
+                <button onClick={decrement}> - </button>
+                <p> { count } </p>
+                <button onClick={increment}> + </button>
+            </div>
+            <button onClick={()=> onAdd(count)}> Agregar al carrito </button>
+        </div>
+    )
+}
+
+export default ItemCount
